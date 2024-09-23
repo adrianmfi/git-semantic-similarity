7a218263eec687605d72206e44086c443bc31136
Adrian Meidell Fiorito <adrianmefi@gmail.com>
2024-09-23 19:08:05 +0200

    Install a formatter

--- src/cli.py
+++ src/cli.py
@@ -10,19 +10,20 @@ from git.exc import InvalidGitRepositoryError
 
 from src.embeddings import embed_commit, embed_query, load_model
 
+
 def parse_git_args(remainder):
     git_args = {}
     it = iter(remainder)  # Create an iterator for the remainder list
     for arg in it:
-        if arg.startswith('-'):
+        if arg.startswith("-"):
             # Remove leading dashes and split if it contains '='
-            key, eq, val = arg.lstrip('-').partition('=')
+            key, eq, val = arg.lstrip("-").partition("=")
             if eq:  # If the argument is in the format -key=value or --key=value
                 git_args[key] = val
             else:  # If the argument is in the format -key value or --key value
                 # Peek next item to see if it's a value or another key
                 next_arg = next(it, None)
-                if next_arg and not next_arg.startswith('-'):
+                if next_arg and not next_arg.startswith("-"):
                     git_args[key] = next_arg
                 else:
                     git_args[key] = True  # Handle flags without explicit value
@@ -37,7 +38,7 @@ def parse_git_args(remainder):
 
 
 def sanitize_filename(filename):
-    return re.sub(r'[<>:"/\\|?*]', '_', filename)
+    return re.sub(r'[<>:"/\\|?*]', "_", filename)
 
 
 class Commit(TypedDict):
@@ -48,56 +49,67 @@ class Commit(TypedDict):
 def main():
     try:
         parser = argparse.ArgumentParser(
-            prog='git-semsim',
-            description='Give a similarity score for each commit, based on the semantic similarity from an NLP embedding model')
+            prog="git-semsim",
+            description="Give a similarity score for each commit, based on the semantic similarity from an NLP embedding model",
+        )
 
         parser.add_argument(
-            '-m', '--model',
-            choices=['sentence-transformers/all-MiniLM-L6-v2'],
-            default='sentence-transformers/all-MiniLM-L6-v2',
-            help="Model to use")
+            "-m",
+            "--model",
+            choices=["sentence-transformers/all-MiniLM-L6-v2"],
+            default="sentence-transformers/all-MiniLM-L6-v2",
+            help="Model to use",
+        )
 
         parser.add_argument(
-            '-s', '--save',
-            default=True,
-            type=bool,
-            help="Save commit embeddings")
+            "-s", "--save", default=True, type=bool, help="Save commit embeddings"
+        )
 
         parser.add_argument(
-            '--save-path',
-            help="Path to save embeddings to (default git_root/.git_semsim/model_name)")
+            "--save-path",
+            help="Path to save embeddings to (default git_root/.git_semsim/model_name)",
+        )
 
-        parser.add_argument('query',
-                            help="The string to compare with")
-        parser.add_argument('git_args', nargs=argparse.REMAINDER,
-                            help="Arguments to forward to git rev-list (for example --author bob --max-count 10)")
+        parser.add_argument("query", help="The string to compare with")
+        parser.add_argument(
+            "git_args",
+            nargs=argparse.REMAINDER,
+            help="Arguments to forward to git rev-list (for example --author bob --max-count 10)",
+        )
 
         args = parser.parse_args()
         git_args = parse_git_args(args.git_args)
         try:
-            repo = Repo('.', search_parent_directories=True)
+            repo = Repo(".", search_parent_directories=True)
         except InvalidGitRepositoryError:
             print("Not a valid git repository")
             sys.exit(1)
 
-        commits: List[Commit] = [{'hexsha': commit.hexsha, 'message': commit.message}
-                                 for commit in repo.iter_commits(**git_args)]
+        commits: List[Commit] = [
+            {"hexsha": commit.hexsha, "message": commit.message}
+            for commit in repo.iter_commits(**git_args)
+        ]
 
-        model = load_model('sentence-transformers/all-MiniLM-L6-v2')
+        model = load_model("sentence-transformers/all-MiniLM-L6-v2")
         query_embedding = embed_query(model, args.query)
 
         save_path = args.save_path
         if not save_path:
             save_path = os.path.join(
-                repo.working_tree_dir,  '.git_semsim', sanitize_filename(args.model))
+                repo.working_tree_dir, ".git_semsim", sanitize_filename(args.model)
+            )
         if args.save:
             os.makedirs(save_path, exist_ok=True)
 
         for commit in commits:
-            commit_embedding = embed_commit(
-                model, commit,  args.save, save_path)
+            commit_embedding = embed_commit(model, commit, args.save, save_path)
             similarity = np.dot(commit_embedding, query_embedding)
-            print(str(similarity) + "\t"+commit['hexsha'] +
-                  " "+commit['message'].splitlines()[0])
+            print(
+                str(similarity)
+                + "\t"
+                + commit["hexsha"]
+                + " "
+                + commit["message"].splitlines()[0]
+            )
     except KeyboardInterrupt:
         sys.exit(0)
